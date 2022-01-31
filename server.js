10725e4b03e107491e523b30ca10614a9c609c59
chiarabbb <90757618+chiarabbb@users.noreply.github.com>
2022-01-31 18:07:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,4 +15,26 @@ app.use(express.static("public"));
 //  Import socket
 let serverSocket = require("socket.io");
 //  Assign the variable that runs the express
-let io = se
+let io = serverSocket(server);
+let connectedSockets = 0;
+//  On connection run the newConnection() function
+io.on("connection", newConnection);
+io.on("disconnect", () => {
+  connectedSockets--;
+});
+
+//  What happens when a new client connects
+function newConnection(newSocket) {
+  console.log(newSocket.id);
+  connectedSockets++;
+  // console.log("connectedSockets:", connectedSockets);
+  newSocket.emit("number", connectedSockets);
+  newSocket.on("mouse", mouseMessage);
+  // newSocket.broadcast.emit("playerBroadcast", connectedSockets);
+
+  function mouseMessage(message) {
+    console.log("message:", message);
+    //  Breadcasting the meassage to all the other clients
+    newSocket.broadcast.emit("mouseBroadcast", message);
+  }
+}
